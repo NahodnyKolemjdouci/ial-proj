616ff1eb67a43330a03a1ec4cbce8ea87aff3839
Jakub <kolbjakub@gmail.com>
2018-12-03 20:37:48 +0100

    Uprava Dijkstra

--- proj.c
+++ proj.c
@@ -30,7 +30,13 @@ char *str_reverse(const char *const str) {
 }
 
 int dijsktra(int **cost, int source, int target, int size) {
-    int dist[size], prev[size], selected[size], i,start, m, min, d, j;
+    int dist[size], prev[size],
+    selected[size],
+    i, start,
+    m,
+            min,
+    d,
+            j;
     char path[size];
     for (i = 0; i < size; i++) {
         dist[i] = IN;
@@ -40,9 +46,9 @@ int dijsktra(int **cost, int source, int target, int size) {
     start = source;
     selected[start] = 1;
     dist[start] = 0;
-   // while (selected[target] == 0) {
-    j=0;
-    while (j<size) {
+    // while (selected[target] == 0) {
+    j = 0;
+   /* while (j<size-1) {
         min = IN;
         m = 0;
         for (i = 0; i < size; i++) {
@@ -59,6 +65,26 @@ int dijsktra(int **cost, int source, int target, int size) {
         start = m;
         j++;
         selected[start] = 1;
+    }*/
+    while (j<size-1) {
+        min = IN;
+        m = 0;
+        for (i = 0; i < size; i++) {
+            if(cost[i][start] != 0 ) {
+                d = dist[start] + cost[i][start];
+                if (d < dist[i] && selected[i] == 0) {
+                    dist[i] = d;
+                    prev[i] = start;
+                }
+                if (min > dist[i] && selected[i] == 0) {
+                    min = dist[i];
+                    m = i;
+                }
+            }
+        }
+        start = m;
+        j++;
+        selected[start] = 1;
     }
     int tmp = target;
     j = 0;
@@ -68,7 +94,7 @@ int dijsktra(int **cost, int source, int target, int size) {
     }
     path[j] = '\0';
     str_reverse(path);
-    if(dist[tmp] == IN){
+    if (dist[tmp] == IN) {
 
         fprintf(stderr, "Cesta neexistuje\n");
         return (-1);
@@ -82,8 +108,8 @@ void print_help() {
     printf("Použití: \n ./proj + [graf.txt] +[-b(Bellman Ford)|-d(Dijkstra)]\n");
 }
 
-int *allocate_mem(int*** arr, int n, int m) {
-    *arr = (int**)malloc(n * sizeof(int*));
+int *allocate_mem(int ***arr, int n, int m) {
+    *arr = (int **) malloc(n * sizeof(int *));
     int *arr_data = malloc(n * m * sizeof(int));
     for (int i = 0; i < n; i++) {
         (*arr)[i] = arr_data + i * m;
@@ -91,7 +117,7 @@ int *allocate_mem(int*** arr, int n, int m) {
     return arr_data; //free point
 }
 
-void deallocate_mem(int*** arr) {
+void deallocate_mem(int ***arr) {
     free(*arr);
     *arr = NULL;
 }
@@ -106,7 +132,7 @@ int count_lines(char *filename) {
     char ch, last;
     while (!feof(file)) {
         ch = fgetc(file);
-        if (ch == '\n' && last !='\n') {
+        if (ch == '\n' && last != '\n') {
             lines++;
         }
         last = ch;
@@ -159,7 +185,7 @@ int **get_matrix(char *filename, int size) {
     }
 
     while ((line = fgets(buffer, sizeof(buffer), file)) != NULL) {
-        if(line[0] == '\n'){
+        if (line[0] == '\n') {
             break;
         }
         record = strtok(line, " ,;");
@@ -207,7 +233,7 @@ int BellmanFord(int **cost, int size, int target, int source) {
         }
     }
 
-    if(distance[source] == IN){
+    if (distance[source] == IN) {
 
         fprintf(stderr, "Cesta neexistuje\n");
         return (-1);
@@ -226,20 +252,20 @@ int main(int argc, char *argv[]) {
         int source2, target2;
         char source, target;
         if (size > 0) {
-            int** matrix = get_matrix(argv[1], size);
+            int **matrix = get_matrix(argv[1], size);
             printf("Matrix: %s \n", argv[1]);
             printf("Pocet uzlu grafu: %d\n", size);
             printout_matrix(matrix, size, size);
 
 
             // nastavim vsechny 0 na IN(999)
-            for (int i = 0; i < size; i++) {
+            /*for (int i = 0; i < size; i++) {
                 for (int j = 0; j < i; j++) {
                     if (matrix[i][j] == 0) {
                         matrix[i][j] = matrix[j][i] = IN;
                     }
                 }
-            }
+            }*/
 
             printf("\nZadejte pocatecni misto:");
             scanf("%s", &target);
@@ -263,7 +289,7 @@ int main(int argc, char *argv[]) {
             }
             co = dijsktra(matrix, source2, target2, size);
 
-            if(co>=0) {
+            if (co >= 0) {
                 printf("\nNejkratsi cesta: %d\n", co);
             }
             deallocate_mem(&matrix);
@@ -278,7 +304,7 @@ int main(int argc, char *argv[]) {
         int co;
         int source2, target2;
         char source, target;
-        int** matrix = get_matrix(argv[1], size);
+        int **matrix = get_matrix(argv[1], size);
         printf("Matrix: %s \n", argv[1]);
         printf("Pocet uzlu grafu: %d\n", size);
         printout_matrix(matrix, size, size);
@@ -305,8 +331,11 @@ int main(int argc, char *argv[]) {
         }
 
         co = BellmanFord(matrix, size, target2, source2);
-        if(co>=0) {
+        if (co >= 0) {
             printf("\nNejkratsi cesta: %d\n", co);
+        } else {
+            fprintf(stderr, "Cesta neexistuje\n");
+            return (-1);
         }
         deallocate_mem(&matrix);
     } else {
